6108c2c94621d3e941d911939f4464d0771542b5
darryl chandra <darrylchan05@gmail.com>
2025-07-31 23:00:27 +0700

    added update

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectToDatabase = require("../models/db");
 const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const {body, validationResult} = require("express-validator");
+const logger = require("../logger");
 
 
 const router = express.Router();
@@ -116,4 +118,64 @@ router.post("/login", async (req, res, next) => {
   });
   return res.status(200).json({ message: "Login successful" });
 })
+
+router.put('/update', async (req, res) => {
+  // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    logger.error('Validation errors', errors.array());
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+  // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+  const email = req.headers.email;
+  if (!email) {
+    logger.error('Email header is missing');
+    return res.status(400).json({ message: 'Email header is required' });
+  }
+  // Task 4: Connect to MongoDB
+  let temp;
+  try{
+    temp = await connectToDatabase();
+  } catch (e) {
+    logger.error('Failed to connect to database', e);
+    return res.status(500).send('Internal server error');
+  }
+  const db = temp;
+  temp = null;
+  const collection = db.collection('users');
+  // Task 5: Find the user credentials in database
+  const existingUser = collection.findOne({ email: email });
+  if (!existingUser) {
+    logger.error('User not found', { email });
+    return res.status(404).json({ message: 'User not found' });
+  }
+  if(!bcrypt.compare(req.body.password, existingUser.hashedPassword)){
+    logger.error('Incorrect password for user', { email });
+    return res.status(400).json({ message: 'Incorrect password' });
+  }
+  existingUser.firstName = req.body.name || existingUser.firstName;
+  existingUser.updatedAt = new Date();
+  // Task 6: Update the user credentials in the database
+  collection.updateOne(
+    { _id: existingUser._id },
+    { $set: existingUser }
+  );
+  // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+  const jwtPayload = {
+    user: {
+      id: existingUser._id,
+    },
+  };
+  const authtoken = jsonwebtoken.sign(
+    jwtPayload,
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+  res.json({authtoken});
+} catch (e) {
+   return res.status(500).send('Internal server error');
+
+}
+});
 module.exports = router;
