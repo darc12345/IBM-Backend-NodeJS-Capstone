62fc0dec8c4fa615e65d63e519533369a7d924d8
darryl chandra <darrylchan05@gmail.com>
2025-08-02 16:48:41 +0700

    linted

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,183 +1,183 @@
-const express = require("express");
-const connectToDatabase = require("../models/db");
-const jsonwebtoken = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const { validationResult } = require("express-validator");
-const logger = require("../logger");
+const express = require('express')
+const connectToDatabase = require('../models/db')
+const jsonwebtoken = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { validationResult } = require('express-validator')
+const logger = require('../logger')
 
-const router = express.Router();
-router.post("/register", async (req, res) => {
-  let db;
+const router = express.Router()
+router.post('/register', async (req, res) => {
+  let db
   try {
-    db = await connectToDatabase();
+    db = await connectToDatabase()
   } catch (e) {
-    logger.error("Failed to connect to DB", e);
-    return res.status(500).json({ message: "Internal Server Error" });
+    logger.error('Failed to connect to DB', e)
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  const collection = db.collection("users");
-  const { email, password, firstName, lastName } = req.body;
-  //check if the credential already exists
+  const collection = db.collection('users')
+  const { email, password, firstName, lastName } = req.body
+  // check if the credential already exists
   try {
-    let prior_account = await collection.findOne({ email: email });
-    console.log("prior_account:", prior_account);
+    const prior_account = await collection.findOne({ email })
+    console.log('prior_account:', prior_account)
     if (prior_account) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: 'User already exists' })
     }
   } catch (e) {
-    logger.error("Error checking existing user", e);
-    return res.status(500).json({ message: "Internal Server Error" });
+    logger.error('Error checking existing user', e)
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  //insert the new user
+  // insert the new user
   if (!email || !password || !firstName || !lastName) {
-    return res.status(400).json({ message: "Email and password are required" });
+    return res.status(400).json({ message: 'Email and password are required' })
   }
   // Hash the password before storing it
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10)
   const newUser = {
-    email: email,
-    hashedPassword: hashedPassword,
-    firstName: firstName,
-    lastName: lastName,
+    email,
+    hashedPassword,
+    firstName,
+    lastName,
     createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-  let insertResult;
+    updatedAt: new Date()
+  }
+  let insertResult
   try {
-    insertResult = await collection.insertOne(newUser);
+    insertResult = await collection.insertOne(newUser)
     if (!insertResult.acknowledged) {
-      return res.status(500).json({ message: "Failed to register user" });
+      return res.status(500).json({ message: 'Failed to register user' })
     }
   } catch (e) {
-    logger.error("Error inserting new user", e);
-    return res.status(500).json({ message: "Internal Server Error" });
+    logger.error('Error inserting new user', e)
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  //setting the JWT token
+  // setting the JWT token
   const jwtPayload = {
     user: {
-      id: insertResult.insertedId,
-    },
-  };
+      id: insertResult.insertedId
+    }
+  }
   try {
     const authToken = jsonwebtoken.sign(jwtPayload, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.cookie("authToken", authToken, {
+      expiresIn: '1h'
+    })
+    res.cookie('authToken', authToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
-    });
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'Strict'
+    })
   } catch (e) {
-    logger.error("Error signing JWT", e);
-    return res.status(500).json({ message: "Internal Server Error" });
+    logger.error('Error signing JWT', e)
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  return res.status(201).json({ message: "User registered successfully" });
-});
-router.post("/login", async (req, res, ) => {
+  return res.status(201).json({ message: 'User registered successfully' })
+})
+router.post('/login', async (req, res) => {
   try {
-    temp = await connectToDatabase();
+    temp = await connectToDatabase()
   } catch (e) {
-    console.error("Failed to connect to DB", e);
-    return res.status(500).json({ message: "Internal Server Error" });
+    console.error('Failed to connect to DB', e)
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  const db = temp;
-  const { email, password } = req.body;
+  const db = temp
+  const { email, password } = req.body
   if (!email || !password) {
-    return res.status(400).json({ message: "Email and password are required" });
+    return res.status(400).json({ message: 'Email and password are required' })
   }
-  const collection = db.collection("users");
-  let temp;
+  const collection = db.collection('users')
+  let temp
   try {
-    temp = await collection.findOne({ email: email });
+    temp = await collection.findOne({ email })
   } catch (e) {
-    console.error("Error finding user", e);
-    return res.status(500).json({ message: "Internal Server Error" });
+    console.error('Error finding user', e)
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  const userResult = temp;
-  temp = null;
+  const userResult = temp
+  temp = null
   if (!userResult) {
-    return res.status(400).json({ message: "Invalid email or password" });
+    return res.status(400).json({ message: 'Invalid email or password' })
   }
   // Verify the password
   const isPasswordValid = await bcrypt.compare(
     password,
     userResult.hashedPassword
-  );
+  )
   if (!isPasswordValid) {
-    return res.status(400).json({ message: "Invalid email or password" });
+    return res.status(400).json({ message: 'Invalid email or password' })
   }
   // Create JWT token
   const jwtPayload = {
     user: {
-      id: userResult._id,
-    },
-  };
+      id: userResult._id
+    }
+  }
   const authtoken = jsonwebtoken.sign(jwtPayload, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-  res.cookie("authToken", authtoken, {
+    expiresIn: '1h'
+  })
+  res.cookie('authToken', authtoken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
-  });
-  const userName = userResult.firstName;
-  const userEmail = userResult.email;
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict'
+  })
+  const userName = userResult.firstName
+  const userEmail = userResult.email
   return res
     .status(200)
-    .json({ authtoken: authtoken, userName: userName, userEmail: userEmail });
-});
+    .json({ authtoken, userName, userEmail })
+})
 
-router.put("/update", async (req, res) => {
+router.put('/update', async (req, res) => {
   // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    logger.error("Validation errors", errors.array());
-    return res.status(400).json({ errors: errors.array() });
+    logger.error('Validation errors', errors.array())
+    return res.status(400).json({ errors: errors.array() })
   }
   try {
     // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
-    const email = req.headers.email;
+    const email = req.headers.email
     if (!email) {
-      logger.error("Email header is missing");
-      return res.status(400).json({ message: "Email header is required" });
+      logger.error('Email header is missing')
+      return res.status(400).json({ message: 'Email header is required' })
     }
     // Task 4: Connect to MongoDB
-    let temp;
+    let temp
     try {
-      temp = await connectToDatabase();
+      temp = await connectToDatabase()
     } catch (e) {
-      logger.error("Failed to connect to database", e);
-      return res.status(500).send("Internal server error");
+      logger.error('Failed to connect to database', e)
+      return res.status(500).send('Internal server error')
     }
-    const db = temp;
-    temp = null;
-    const collection = db.collection("users");
+    const db = temp
+    temp = null
+    const collection = db.collection('users')
     // Task 5: Find the user credentials in database
-    const existingUser = collection.findOne({ email: email });
+    const existingUser = collection.findOne({ email })
     if (!existingUser) {
-      logger.error("User not found", { email });
-      return res.status(404).json({ message: "User not found" });
+      logger.error('User not found', { email })
+      return res.status(404).json({ message: 'User not found' })
     }
     if (!bcrypt.compare(req.body.password, existingUser.hashedPassword)) {
-      logger.error("Incorrect password for user", { email });
-      return res.status(400).json({ message: "Incorrect password" });
+      logger.error('Incorrect password for user', { email })
+      return res.status(400).json({ message: 'Incorrect password' })
     }
-    existingUser.firstName = req.body.name || existingUser.firstName;
-    existingUser.updatedAt = new Date();
+    existingUser.firstName = req.body.name || existingUser.firstName
+    existingUser.updatedAt = new Date()
     // Task 6: Update the user credentials in the database
-    collection.updateOne({ _id: existingUser._id }, { $set: existingUser });
+    collection.updateOne({ _id: existingUser._id }, { $set: existingUser })
     // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
     const jwtPayload = {
       user: {
-        id: existingUser._id,
-      },
-    };
+        id: existingUser._id
+      }
+    }
     const authtoken = jsonwebtoken.sign(jwtPayload, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.json({ authtoken });
+      expiresIn: '1h'
+    })
+    res.json({ authtoken })
   } catch (e) {
-    logger.error("Error updating user", e);
-    return res.status(500).send("Internal server error");
+    logger.error('Error updating user', e)
+    return res.status(500).send('Internal server error')
   }
-});
-module.exports = router;
+})
+module.exports = router
--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -1,124 +1,122 @@
-const express = require('express');
-const multer = require('multer');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-const logger = require('../logger');
-const { ReturnDocument } = require('mongodb');
+const express = require('express')
+const multer = require('multer')
+const router = express.Router()
+const connectToDatabase = require('../models/db')
+const logger = require('../logger')
+const { ReturnDocument } = require('mongodb')
 
 // Define the upload directory path
-const directoryPath = 'public/images';
+const directoryPath = 'public/images'
 
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, directoryPath); // Specify the upload directory
+    cb(null, directoryPath) // Specify the upload directory
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
-  },
-});
+    cb(null, file.originalname) // Use the original file name
+  }
+})
 
-const upload = multer({ storage: storage });
-
+const upload = multer({ storage })
 
 // Get all secondChanceItems
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const secondChanceItems = await collection.find({}).toArray();
-        res.status(200).json(secondChanceItems);
-    } catch (e) {
-        console.error('oops something went wrong', e)
-        next(e);
-    }
-});
+  logger.info('/ called')
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection('secondChanceItems')
+    const secondChanceItems = await collection.find({}).toArray()
+    res.status(200).json(secondChanceItems)
+  } catch (e) {
+    console.error('oops something went wrong', e)
+    next(e)
+  }
+})
 
 // Add a new item
-router.post('/', upload.single('file'),async(req, res,next) => {
-    try {
-        
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const secondChanceItem = req.body;
-        const new_id = collection.find().sort({id:-1}).limit(1).toArray()[0]['_id'] + 1;
-        const date_added = Math.floor(new Date().getTime() / 1000);
-        secondChanceItem.id = new_id;
-        secondChanceItem.date_added = date_added;
-        await collection.insertOne(secondChanceItem);
+router.post('/', upload.single('file'), async (req, res, next) => {
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection('secondChanceItems')
+    const secondChanceItem = req.body
+    const new_id = collection.find().sort({ id: -1 }).limit(1).toArray()[0]._id + 1
+    const date_added = Math.floor(new Date().getTime() / 1000)
+    secondChanceItem.id = new_id
+    secondChanceItem.date_added = date_added
+    await collection.insertOne(secondChanceItem)
 
-        res.status(201).json(secondChanceItem.ops[0]);
-    } catch (e) {
-        next(e);
-    }
-});
+    res.status(201).json(secondChanceItem.ops[0])
+  } catch (e) {
+    next(e)
+  }
+})
 
 // Get a single secondChanceItem by ID
 router.get('/:id', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const id = req.params.id;
-        console.log(id)
-        console.log(typeof id)
-        const secondChanceItem = await collection.findOne({ id: id });
-        if (!secondChanceItem) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        res.status(200).json(secondChanceItem);
-    } catch (e) {
-        next(e);
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection('secondChanceItems')
+    const id = req.params.id
+    console.log(id)
+    console.log(typeof id)
+    const secondChanceItem = await collection.findOne({ id })
+    if (!secondChanceItem) {
+      return res.status(404).json({ message: 'Item not found' })
     }
-});
+    res.status(200).json(secondChanceItem)
+  } catch (e) {
+    next(e)
+  }
+})
 
 // Update and existing item
-router.put('/:id', async(req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const id = req.params.id;
-        const exist = collection.findOne({ id: id });
-        if (!exist) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        const updatedItem = req.body;
-        var secondChanceItem = {};
-        secondChanceItem.category = req.body.category;
-        secondChanceItem.condition = req.body.condition;
-        secondChanceItem.age_days = req.body.age_days;
-        secondChanceItem.description = req.body.description;
-        secondChanceItem.age_years = Number((secondChanceItem.age_days/365).toFixed(1));
-        secondChanceItem.updatedAt = new Date();
-
-        const updatePreloveItem = await collection.findOneAndUpdate(
-            { id: id},
-            { $set: updatedItem },
-            {ReturnDocument: ReturnDocument.AFTER }
-        )
-        if (!updatePreloveItem) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        return res.status(200).json(updatePreloveItem.value);
-    } catch (e) {
-        logger.error('Error updating item', e);
+router.put('/:id', async (req, res) => {
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection('secondChanceItems')
+    const id = req.params.id
+    const exist = collection.findOne({ id })
+    if (!exist) {
+      return res.status(404).json({ message: 'Item not found' })
     }
-});
+    const updatedItem = req.body
+    const secondChanceItem = {}
+    secondChanceItem.category = req.body.category
+    secondChanceItem.condition = req.body.condition
+    secondChanceItem.age_days = req.body.age_days
+    secondChanceItem.description = req.body.description
+    secondChanceItem.age_years = Number((secondChanceItem.age_days / 365).toFixed(1))
+    secondChanceItem.updatedAt = new Date()
+
+    const updatePreloveItem = await collection.findOneAndUpdate(
+      { id },
+      { $set: updatedItem },
+      { ReturnDocument: ReturnDocument.AFTER }
+    )
+    if (!updatePreloveItem) {
+      return res.status(404).json({ message: 'Item not found' })
+    }
+    return res.status(200).json(updatePreloveItem.value)
+  } catch (e) {
+    logger.error('Error updating item', e)
+  }
+})
 
 // Delete an existing item
-router.delete('/:id', async(req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("secondChanceItems");
-        const id = req.params.id;
-        const result = await collection.deleteOne({ id: id });
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        res.status(204).send();
-    } catch (e) {
-        logger.error('Error deleting item', e);
+router.delete('/:id', async (req, res) => {
+  try {
+    const db = await connectToDatabase()
+    const collection = db.collection('secondChanceItems')
+    const id = req.params.id
+    const result = await collection.deleteOne({ id })
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: 'Item not found' })
     }
-});
+    res.status(204).send()
+  } catch (e) {
+    logger.error('Error deleting item', e)
+  }
+})
 
-module.exports = router;
+module.exports = router
