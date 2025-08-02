6a3a94060587422bda5f904293631ce5acb0b578
darryl chandra <darrylchan05@gmail.com>
2025-08-02 15:42:16 +0700

    fixed login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -2,16 +2,15 @@ const express = require("express");
 const connectToDatabase = require("../models/db");
 const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const {body, validationResult} = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const logger = require("../logger");
-
 
 const router = express.Router();
 router.post("/register", async (req, res, next) => {
   try {
     db = await connectToDatabase();
   } catch (e) {
-    console.error("Failed to connect to DB", e);
+    logger.error("Failed to connect to DB", e);
     return res.status(500).json({ message: "Internal Server Error" });
   }
   const collection = db.collection("users");
@@ -19,11 +18,12 @@ router.post("/register", async (req, res, next) => {
   //check if the credential already exists
   try {
     let prior_account = await collection.findOne({ email: email });
-    if (Object.keys(prior_account).length > 0) {
+    console.log("prior_account:", prior_account);
+    if (prior_account) {
       return res.status(400).json({ message: "User already exists" });
     }
   } catch (e) {
-    console.error("Error checking existing user", e);
+    logger.error("Error checking existing user", e);
     return res.status(500).json({ message: "Internal Server Error" });
   }
   //insert the new user
@@ -32,8 +32,8 @@ router.post("/register", async (req, res, next) => {
   }
   // Hash the password before storing it
   hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = { 
-    email:email,
+  const newUser = {
+    email: email,
     hashedPassword: hashedPassword,
     firstName: firstName,
     lastName: lastName,
@@ -47,7 +47,7 @@ router.post("/register", async (req, res, next) => {
       return res.status(500).json({ message: "Failed to register user" });
     }
   } catch (e) {
-    console.error("Error inserting new user", e);
+    logger.error("Error inserting new user", e);
     return res.status(500).json({ message: "Internal Server Error" });
   }
   //setting the JWT token
@@ -56,19 +56,17 @@ router.post("/register", async (req, res, next) => {
       id: insertResult.insertedId,
     },
   };
-  try{
-    const authToken = jsonwebtoken.sign(
-      jwtPayload,
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
+  try {
+    const authToken = jsonwebtoken.sign(jwtPayload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
     res.cookie("authToken", authToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "Strict",
     });
   } catch (e) {
-    console.error("Error signing JWT", e);
+    logger.error("Error signing JWT", e);
     return res.status(500).json({ message: "Internal Server Error" });
   }
   return res.status(201).json({ message: "User registered successfully" });
@@ -76,20 +74,19 @@ router.post("/register", async (req, res, next) => {
 router.post("/login", async (req, res, next) => {
   try {
     db = await connectToDatabase();
-  }
-  catch (e) {
+  } catch (e) {
     console.error("Failed to connect to DB", e);
     return res.status(500).json({ message: "Internal Server Error" });
   }
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: "Email and password are required" });
   }
   const collection = db.collection("users");
   let temp;
-  try{
+  try {
     temp = await collection.findOne({ email: email });
-  } catch(e) {
+  } catch (e) {
     console.error("Error finding user", e);
     return res.status(500).json({ message: "Internal Server Error" });
   }
@@ -99,7 +96,10 @@ router.post("/login", async (req, res, next) => {
     return res.status(400).json({ message: "Invalid email or password" });
   }
   // Verify the password
-  const isPasswordValid = await bcrypt.compare(password, userResult.hashedPassword);
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    userResult.hashedPassword
+  );
   if (!isPasswordValid) {
     return res.status(400).json({ message: "Invalid email or password" });
   }
@@ -109,76 +109,72 @@ router.post("/login", async (req, res, next) => {
       id: userResult._id,
     },
   };
-  const authToken = jsonwebtoken.sign(
-    jwtPayload,
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
-  res.cookie("authToken", authToken, {
+  const authtoken = jsonwebtoken.sign(jwtPayload, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  res.cookie("authToken", authtoken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "Strict",
   });
-  return res.status(200).json({ message: "Login successful" });
-})
+  const userName = userResult.firstName;
+  const userEmail = userResult.email;
+  return res
+    .status(200)
+    .json({ authtoken: authtoken, userName: userName, userEmail: userEmail });
+});
 
-router.put('/update', async (req, res) => {
+router.put("/update", async (req, res) => {
   // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    logger.error('Validation errors', errors.array());
+    logger.error("Validation errors", errors.array());
     return res.status(400).json({ errors: errors.array() });
   }
   try {
-  // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
-  const email = req.headers.email;
-  if (!email) {
-    logger.error('Email header is missing');
-    return res.status(400).json({ message: 'Email header is required' });
-  }
-  // Task 4: Connect to MongoDB
-  let temp;
-  try{
-    temp = await connectToDatabase();
+    // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+    const email = req.headers.email;
+    if (!email) {
+      logger.error("Email header is missing");
+      return res.status(400).json({ message: "Email header is required" });
+    }
+    // Task 4: Connect to MongoDB
+    let temp;
+    try {
+      temp = await connectToDatabase();
+    } catch (e) {
+      logger.error("Failed to connect to database", e);
+      return res.status(500).send("Internal server error");
+    }
+    const db = temp;
+    temp = null;
+    const collection = db.collection("users");
+    // Task 5: Find the user credentials in database
+    const existingUser = collection.findOne({ email: email });
+    if (!existingUser) {
+      logger.error("User not found", { email });
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!bcrypt.compare(req.body.password, existingUser.hashedPassword)) {
+      logger.error("Incorrect password for user", { email });
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+    existingUser.firstName = req.body.name || existingUser.firstName;
+    existingUser.updatedAt = new Date();
+    // Task 6: Update the user credentials in the database
+    collection.updateOne({ _id: existingUser._id }, { $set: existingUser });
+    // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+    const jwtPayload = {
+      user: {
+        id: existingUser._id,
+      },
+    };
+    const authtoken = jsonwebtoken.sign(jwtPayload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ authtoken });
   } catch (e) {
-    logger.error('Failed to connect to database', e);
-    return res.status(500).send('Internal server error');
+    return res.status(500).send("Internal server error");
   }
-  const db = temp;
-  temp = null;
-  const collection = db.collection('users');
-  // Task 5: Find the user credentials in database
-  const existingUser = collection.findOne({ email: email });
-  if (!existingUser) {
-    logger.error('User not found', { email });
-    return res.status(404).json({ message: 'User not found' });
-  }
-  if(!bcrypt.compare(req.body.password, existingUser.hashedPassword)){
-    logger.error('Incorrect password for user', { email });
-    return res.status(400).json({ message: 'Incorrect password' });
-  }
-  existingUser.firstName = req.body.name || existingUser.firstName;
-  existingUser.updatedAt = new Date();
-  // Task 6: Update the user credentials in the database
-  collection.updateOne(
-    { _id: existingUser._id },
-    { $set: existingUser }
-  );
-  // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
-  const jwtPayload = {
-    user: {
-      id: existingUser._id,
-    },
-  };
-  const authtoken = jsonwebtoken.sign(
-    jwtPayload,
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
-  res.json({authtoken});
-} catch (e) {
-   return res.status(500).send('Internal server error');
-
-}
 });
 module.exports = router;
