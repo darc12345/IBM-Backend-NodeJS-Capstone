10936e1415b179d5a7f27b8b3883c21e00534b42
darryl chandra <darrylchan05@gmail.com>
2025-08-02 17:15:20 +0700

    lint fixed

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 8 */
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -44,18 +45,15 @@ app.use('/api/auth', authRoutes)
 // {{insert code here}}
 
 // Global Error Handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err)
   res.status(500).send('Internal Server Error')
-  }
-)
+})
 
 app.get('/', (req, res) => {
   res.send('Inside the server')
-  }
-)
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
-  }
-)
+})
